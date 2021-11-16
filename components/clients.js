0a11567712bdbbc8d00f6d5877b544738c4ce63c
buriedsignals <tom.vaillant@icloud.com>
2021-11-16 12:36:40 +0100

    temp fix

--- components/clients.js
+++ components/clients.js
@@ -6,21 +6,20 @@ class Clients extends React.Component {
       super(props);
       console.log('Initializing clients.')
     }
-  
+    // <img src="../static/logos/hbo.png" />
     render() {
       return (
         <div className="clients-wrapper">
           <h3>THEY WORKED WITH US</h3>
           <div className="clients-row">
             <img src="../static/logos/redbull.png" />
-            <img src="../static/logos/hbo.png" />
+            <img src="../static/logos/on.png" />
             <img src="../static/logos/ibm.png" />
             <img src="../static/logos/riot.png" />
           </div>
           <div className="clients-row">
             <img src="../static/logos/seeds.png" />
             <img src="../static/logos/undp.png" />
-            <img src="../static/logos/on.png" />
             <img src="../static/logos/qatar.png" />
           </div>
         </div>
