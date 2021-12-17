c47647b95334e738223c3f393fb350f9791e7cce
Miha Klasinc <mk4908@nyu.edu>
2021-12-17 11:04:39 +0100

    feat: added tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
 import Head from "next/head";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function BuriedSignalsMag({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+
+        if (window && window._paq) {
+            _paq.push(['setCustomUrl', url]);
+            _paq.push(['trackPageView']);
+        }
+    }
+
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+}, [router.events])
 
   return (
     <div>
--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,18 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400&display=swap"
             rel="stylesheet"
           />
+            <script dangerouslySetInnerHTML={{ __html: `
+              var _paq = window._paq = window._paq || [];
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://buriedsignals.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src='//cdn.matomo.cloud/buriedsignals.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            ` }} />
         </Head>
         <body>
           <Main />
