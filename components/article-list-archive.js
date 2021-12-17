5b462bda1740f0cd50514fedba3fa17c50ac3791
Miha Klasinc <mk4908@nyu.edu>
2021-12-17 13:49:42 +0100

    feat: added pagination component

--- components/article-list-archive.js
+++ components/article-list-archive.js
@@ -1,23 +1,29 @@
-const React = require("react");
+import React, {useMemo, useRef} from 'react';
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Navbar from "./navbar/navbar";
 import Footer from "./footer";
 import Head from "next/head";
 import ArticleCard from "./article-card/article-card";
-import Header from "./header";
+import { GeistProvider } from "@geist-ui/react";
+import { Pagination } from "@geist-ui/react";
+import { usePagination } from "react-use-pagination";
+import Spacer from "../components/spacer/spacer";
 
 const ArticleListRouter = (props) => {
+
   const router = useRouter();
   const { pathname } = useRouter();
+
   let navLogo;
   pathname == "/articles/[category]"
-    ? (navLogo = "../../static/img/nav-logo.png")
-    : (navLogo = "../static/img/nav-logo.png");
+    ? (navLogo = "/img/nav-logo.png")
+    : (navLogo = "/img/nav-logo.png");
   let footerLogo;
   pathname == "/articles/[category]"
-    ? (footerLogo = "../../static/img/bs_footer.png")
-    : (footerLogo = "../static/img/bs_footer.png");
+    ? (footerLogo = "/img/bs_footer.png")
+    : (footerLogo = "/img/bs_footer.png");
+
 
   if (pathname == "/articles/[category]") {
     return (
@@ -29,8 +35,8 @@ const ArticleListRouter = (props) => {
             name="description"
             content="Experiments in visual journalism. Curating and crafting explorable explanations, web documentaries and interfaces."
           />
-          <meta image src="../static/img/meta.png" />
-          <meta property="og:image" content="../static/img/meta.png" />
+          <meta image src="/img/meta.png" />
+          <meta property="og:image" content="/img/meta.png" />
           <title>Articles | Buried Signals</title>
         </Head>
         <h1 className="typography-page-header">Archive</h1>
@@ -41,6 +47,7 @@ const ArticleListRouter = (props) => {
       </div>
     );
   } else {
+
     return (
       <div>
         <Head>
@@ -49,8 +56,8 @@ const ArticleListRouter = (props) => {
             name="description"
             content="Experiments in visual journalism. Curating and crafting explorable explanations, web documentaries and interfaces."
           />
-          <meta image src="../static/img/meta.png" />
-          <meta property="og:image" content="../static/img/meta.png" />
+          <meta image src="/img/meta.png" />
+          <meta property="og:image" content="/img/meta.png" />
           <title>Articles | Buried Signals</title>
         </Head>
         <h1 className="typography-page-header">Archive</h1>
@@ -60,73 +67,102 @@ const ArticleListRouter = (props) => {
   }
 };
 
-class ArticleList extends React.Component {
-  constructor(props) {
-    super(props);
+function ArticleList (props) {
 
-    this.state = {
-      selectedCategory: props.category || "all",
-      path: props.pathname,
-    };
-  }
+  const ref = useRef();
 
-  render() {
-    const { hasError, idyll, updateProps, clickCount, ...props } = this.props;
-    console.log("rendering article list archive", props.category);
-
-    const allArticles = props.articles;
-    const allCategories = [
-      ...new Set(allArticles.flatMap((article) => article.categories)),
-    ];
-
-    allCategories.sort((a, b) => {
+  const allCategories = useMemo(() => {
+    return [
+      ...new Set(props.articles.flatMap((article) => article.categories)),
+    ].sort((a, b) => {
       return a.localeCompare(b);
-    });
+    })
+  }, [props.articles])
 
-    const currentPath = props.path;
-    const selectedCategoryArticles =
-      props.category === "all"
-        ? allArticles
-        : allArticles.filter((article) =>
-            article.categories.includes(props.category)
-          );
+  const selectedCategoryArticles = useMemo(() => {
+    return props.category === "all"
+      ? props.articles
+      : props.articles.filter((article) =>
+          article.categories.includes(props.category)
+        );
+  }, [props.articles, props.category])
 
-    return (
-      <div>
-        <div className="index-filters">
-          <div
-            className="filter-category"
-            data-active={props.category === "all"}
-          >
-            <Link href="/articles" className="filter-category-a">
-              All
-            </Link>
-          </div>
-          {allCategories.map((category) => {
-            return (
-              <div
-                key={category}
-                className="filter-category"
-                data-active={props.category === category}
-              >
-                <Link
-                  href={`/articles/${category}`}
-                  className="filter-category-a"
-                >
-                  {category.toUpperCase()}
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-        <div className="articles-index">
-          {selectedCategoryArticles.map((article) => {
-            return <ArticleCard key={article.slug} article={article} />;
-          })}
-        </div>
-      </div>
-    );
+  const showPagination = useMemo(() => {
+    return selectedCategoryArticles.length > 4
+  }, [selectedCategoryArticles])
+
+  const {
+    totalPages,
+    setPage,
+    startIndex,
+    endIndex
+  } = usePagination({
+    totalItems: selectedCategoryArticles.length,
+    initialPageSize: 4,
+    initialPage: 0
+  });
+
+  const categoryArticlesToShow = useMemo(() => {
+    return showPagination 
+      ? selectedCategoryArticles.slice(startIndex, endIndex + 1)
+      : selectedCategoryArticles
+  }, [selectedCategoryArticles, showPagination, startIndex, endIndex])
+
+  function onPageChange(e) {
+    setPage(e - 1);
+    console.log(ref.current)
+    ref?.current.scrollIntoView();
   }
+
+  return (
+    <div ref={ref}>
+      <div className="index-filters">
+        <div
+          className="filter-category"
+          data-active={props.category === "all"}
+        >
+          <Link href="/articles" className="filter-category-a">
+            All
+          </Link>
+        </div>
+        {allCategories.map((category) => {
+          return (
+            <div
+              key={category}
+              className="filter-category"
+              data-active={props.category === category}
+            >
+              <Link
+                href={`/articles/${category}`}
+                className="filter-category-a"
+              >
+                {category.toUpperCase()}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="articles-index">
+        {categoryArticlesToShow.map((article) => {
+          return <ArticleCard key={article.slug} article={article} />;
+        })}
+      </div>
+
+      {showPagination && (
+        <>
+          <Spacer multiplier={1} />
+          <div id="archive-pagination" className="typography-paragraph">
+            <GeistProvider>
+              <Pagination count={totalPages} limit={3} onChange={onPageChange}></Pagination>
+            </GeistProvider>
+          </div>
+            <Spacer multiplier={1} />
+        </>
+      )}
+      
+    </div>
+  );
 }
 
 export default ArticleListRouter;
