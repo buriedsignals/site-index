a72f6dd61ee41c62d519b18081a8dac2ef4017e1
buriedsignals <tom.vaillant@icloud.com>
2022-02-08 13:12:19 +0100

    trying to fix the open graph tags

--- pages/_app.js
+++ pages/_app.js
@@ -25,12 +25,26 @@ function BuriedSignalsMag({ Component, pageProps }) {
 
   return (
     <div>
-    <Head>
-      <meta charset="UTF-8" />
-      <meta name="keywords" content="titla, meta, nextjs" />
-      <meta name="author" content="Buried Signals" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
+      <Head>
+      <title>Buried Signals</title>
+      <meta charset="utf-8" />
+      <meta name="description" content="A publication curating visual, immersive and interactive journalism."/>
+      <meta name="image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" />
+      <meta itemProp="name" content="Buried Signals" />
+      <meta itemProp="description" content="A publication curating visual, immersive and interactive journalism." />
+      <meta itemProp="image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="Buried Signals" />
+      <meta name="twitter:description" content="A publication curating visual, immersive and interactive journalism." />
+      <meta name="twitter:site" content="@buriedsignals" />
+      <meta name="twitter:image:src" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" />
+      <meta name="og:title" content="Buried Signals" />
+      <meta name="og:description" content="A publication curating visual, immersive and interactive journalism." />
+      <meta name="og:image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" />
+      <meta name="og:url" content="https://buriedsignals.com" />
+      <meta name="og:site_name" content="Buried Signals" />
+      <meta name="og:type" content="website" />
+      </Head>
     <Component {...pageProps} />
     </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -17,18 +17,18 @@ const ArticleCategory = () => {
       <Head>
         <title>Buried Signals</title>
         <meta charset="utf-8" />
-        <meta name="description" content="Curating visual and immersive journalism."/>
+        <meta name="description" content="A publication curating visual, immersive and interactive journalism."/>
         <meta name="image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" />
         <meta itemProp="name" content="Buried Signals" />
-        <meta itemProp="description" content="Curating visual and immersive journalism." />
+        <meta itemProp="description" content="A publication curating visual, immersive and interactive journalism." />
         <meta itemProp="image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="Buried Signals" />
-        <meta name="twitter:description" content="Curating visual and immersive journalism." />
+        <meta name="twitter:description" content="A publication curating visual, immersive and interactive journalism." />
         <meta name="twitter:site" content="@buriedsignals" />
         <meta name="twitter:image:src" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" />
         <meta name="og:title" content="Buried Signals" />
-        <meta name="og:description" content="Curating visual and immersive journalism." />
+        <meta name="og:description" content="A publication curating visual, immersive and interactive journalism." />
         <meta name="og:image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" />
         <meta name="og:url" content="https://buriedsignals.com" />
         <meta name="og:site_name" content="Buried Signals" />
