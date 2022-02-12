6fb710ddf2b20342d8f374cde2a22e85bb47c549
buriedsignals <tom.vaillant@icloud.com>
2022-02-12 22:52:35 +0100

    last seo update

--- pages/about.js
+++ pages/about.js
@@ -25,10 +25,10 @@ const About = () => {
       <Head>
         <title>Buried Signals</title>
         <meta charset="utf-8" />
-        <meta name="description" content="A publication curating visual, immersive and interactive journalism." key="meta_description" />
+        <meta name="description" content="A publication curating visual, interactive and investigative stories." key="meta_description" />
         <meta name="image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" key="meta_image" />
         <meta itemProp="name" content="Buried Signals" />
-        <meta itemProp="description" content="A publication curating visual, immersive and interactive journalism." />
+        <meta itemProp="description" content="A publication curating visual, interactive and investigative stories.." />
         <meta itemProp="image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" />
         <meta name="twitter:card" content="summary_large_image" key="twitter_card" />
         <meta name="twitter:title" content="Buried Signals" key="twitter_title" />
@@ -36,7 +36,7 @@ const About = () => {
         <meta name="twitter:site" content="@buriedsignals" key="twitter_site" />
         <meta name="twitter:image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" key="twitter_image" />
         <meta name="title" property="og:title" content="Buried Signals" key="title" />
-        <meta name="description" property="og:description" content="A publication curating visual, immersive and interactive journalism."  key="description" />
+        <meta name="description" property="og:description" content="A publication curating visual, interactive and investigative stories."  key="description" />
         <meta name="image" property="og:image" content="https://raw.githubusercontent.com/buriedsignalsmag/site-index/main/static/img/meta.png" key="image"></meta>
         <meta name="url" property="og:url" content="https://buriedsignals.com" key="url" />
         <meta name="site_name" property="og:site_name" content="Buried Signals" key="site_name" />
